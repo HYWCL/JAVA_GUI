9b5ed96adb75ec34df6a39b5091ae64bf2c02352
USER-PC <USER-PC@DESKTOP-S3B948L>
2023-09-08 16:49:58 +0900

    Image Practice

--- 0504/src/GUI.java
+++ 0504/src/GUI.java
@@ -1,18 +1,39 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class GUI {
     public static void main(String[] args) {
         // 프레임 생성
         JFrame frame = new JFrame("간단한 GUI 예제");
+        
+        // 파일 선택 생성
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("이미지 파일", "jpg", "png", "gif"));
 
         // 레이블 생성
-        JLabel label = new JLabel("Hello, World!");
-
-        // 레이블을 프레임에 추가
-        frame.add(label);
+        JLabel label = new JLabel();
+        
+        JButton chooseButton = new JButton("이미지 선택");
+        chooseButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    ImageIcon icon = new ImageIcon(selectedFile.getPath());
+                    label.setIcon(icon);
+                    frame.pack(); // 이미지 크기에 맞게 창 크기 조정
+                }
+            }
+        });
+        
+        // 파일 선택 버튼을 프레임에 추가
+        frame.add(chooseButton, BorderLayout.SOUTH);
 
         // 프레임 크기 설정
-        frame.setSize(300, 100);
+        frame.setSize(400, 400);
 
         // 프레임을 화면 중앙에 표시
         frame.setLocationRelativeTo(null);
